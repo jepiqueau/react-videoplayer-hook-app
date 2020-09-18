b6d55c38f894c8d76078aa28d8adbc37c4fac571
jepiqueau <jepi.queau@free.fr>
2020-09-18 13:36:10 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-# react-datastoragesqlite-app
+# react-videoplayer-hook-app
 Sample project that shows an integration of
- - [capacitor-data-storage-sqlite] (https://github.com/jepiqueau/capacitor-data-storage-sqlite/blob/master/readme.md)
 
- - [react-data-storage-sqlite-hook] (https://www.npmjs.com/package/react-data-storage-sqlite-hook)
+ - [capacitor-video-player] (https://github.com/jepiqueau/capacitor-video-player/blob/master/readme.md)
 
-in React App.
+ - [react-video-player-hook] (https://www.npmjs.com/package/react-video-player-hook)
+
+in a React App.
 
 
 
@@ -14,14 +15,15 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 To start clone the project
 ```bash
-git clone https://github.com/jepiqueau/react-datastoragesqlite-app.git 
-cd react-datastoragesqlite-app
+git clone https://github.com/jepiqueau/react-videoplayer-hook-app.git 
+cd react-videoplayer-hook-app
 git remote rm origin
 npm install
 ```
 
 
-To install the latest release of the ```capacitor-data-storage-sqlite``` plugin
+To install the latest release of the `capacitor-video-player` plugin
+and the latest release of the `react-video-player-hook` hook 
 
 ```bash
 npm run update
@@ -54,5 +56,26 @@ npx cap open android
 ### ELECTRON
 
 ```
-npx cap open @capacitor-community/eletron
+npx cap open @capacitor-community/electron
 ```
+
+## Contributors ✨
+
+Thanks goes to these wonderful people ([emoji key](https://allcontributors.org/docs/en/emoji-key)):
+
+<!-- ALL-CONTRIBUTORS-LIST:START - Do not remove or modify this section -->
+<!-- prettier-ignore-start -->
+<!-- markdownlint-disable -->
+<table>
+  <tr>
+    <td align="center"><a href="https://github.com/jepiqueau"><img src="https://avatars3.githubusercontent.com/u/16580653?v=4" width="100px;" alt=""/><br /><sub><b>Jean Pierre Quéau</b></sub></a><br /><a href="https://github.com/jepiqueau/react-videoplayer-hook-app/commits?author=jepiqueau" title="Code">💻</a></td>
+  </tr>
+</table>
+
+<!-- markdownlint-enable -->
+<!-- prettier-ignore-end -->
+
+<!-- ALL-CONTRIBUTORS-LIST:END -->
+
+This project follows the [all-contributors](https://github.com/all-contributors/all-contributors) specification. Contributions of any kind welcome!
+
--- src/App.js
+++ src/App.js
@@ -1,37 +1,171 @@
-import React from "react";
-import { BrowserRouter, Route } from 'react-router-dom';
+import React from 'react'
+import { Capacitor } from '@capacitor/core';
+import { useState, useEffect, useRef } from 'react';
+import { useVideoPlayer } from 'react-video-player-hook';
 
-import ReactDOM from "react-dom";
-import "./App.css";
-import Home from './components/Home';
-import StoreDefault from './components/StoreDefault';
-import StoreOneDBOneTable from './components/StoreOneDBOneTable';
-import StoreOneDBTwoTables from './components/StoreOneDBTwoTables';
-import StoreTwoDBsTwoTables from './components/StoreTwoDBsTwoTables';
 
-class App extends React.Component{
-  render() {
+const VideoPlayer = () => {
+    const [url, setUrl] = useState( undefined );
+    const platform = Capacitor.getPlatform();
+
+    const {pVideoPlayer, initPlayer, isPlaying, pause, play, getDuration, setVolume,
+        getVolume, setMuted, getMuted, setCurrentTime, getCurrentTime, stopAllPlayers} = useVideoPlayer();
+    
+    let apiTimer1 = useRef();
+    let apiTimer2 = useRef();
+    let apiCount = useRef(-1);
+    const [first, setFirst] = useState(true);
+
+    const exitClear = () => {
+        console.log("%%%% in cleanup Timers %%%%")
+        clearTimeout(apiTimer1);
+        clearTimeout(apiTimer2);
+        setFirst(false);
+    }
+    useEffect(  () => {
+        if ( platform === "ios" || platform === "android" ) {
+            // test url from public/assets
+            setUrl( 'public/assets/video/video.mp4' )
+        } else {
+            // test url from http:
+            setUrl( 'http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_60fps_normal.mp4' )
+        }
+    }, [platform, url] )
+    
+
+    useEffect( () => {
+        let playListener = null;
+        let pauseListener = null;
+        let readyListener = null;
+        let exitListener = null;
+        let endedListener = null;
+        if(first && pVideoPlayer && url ) {
+            // test mode "embedded" for video player on Web platform
+            const playerWeb = async () => {
+                let res;
+                res = await initPlayer( "embedded", url, "fullscreen-video", 'div', 1280, 720);
+                if ( res.result.result && res.result.value ) {
+                    res = await play( "fullscreen-video" );
+                }
+            }
+            // test mode "fullscreen" for video player on native platform
+            const playerNative = async () => {
+                try {
+                    await initPlayer("fullscreen", url, "fullscreen-video");
+                } catch ( error ) {
+                    console.log( error );
+                }
+            }
+            console.log("pVideoPlayer " + pVideoPlayer)
+            console.log("url " + url)
+            playListener = pVideoPlayer.addListener( 'jeepCapVideoPlayerPlay', async( e ) => { 
+                console.log( "Event jeepCapVideoPlayerPlay playerId " + 
+                e.fromPlayerId + " time " + e.currentTime);
+                const mIsPlaying = await isPlaying(e.fromPlayerId);
+                console.log("==> mIsPlaying " + JSON.stringify(mIsPlaying));
+                apiCount.current += 1;
+                if(apiCount.current === 0) {
+                    const volume = await getVolume(e.fromPlayerId);
+                    if(volume.result) {
+                        console.log("==> volume " + volume.value);
+                    } else {
+                        console.log("==> volume " + volume.message);
+                    }
+                    apiTimer1.current = setTimeout(async () => {
+                        const duration = await getDuration(e.fromPlayerId);
+                        console.log("==> duration " + JSON.stringify(duration));
+                        if(duration.result) {
+                            console.log("==> duration " + duration.value);
+                        } else {
+                            console.log("==> duration " + duration.message);
+                        }
+                            const volume = await setVolume(e.fromPlayerId,0.2);
+                        console.log("====> Volume ",volume.value);
+                        const currentTime = await getCurrentTime(e.fromPlayerId);
+                        if(currentTime.result) {
+                            console.log('==> currentTime ' + currentTime.value);
+                            const seektime = currentTime.value + 0.4 * duration.value; 
+                            console.log("seektime" + seektime)
+                            const sCurrentTime = await setCurrentTime(e.fromPlayerId,seektime);
+                            console.log("==> setCurrentTime " + sCurrentTime.value);
+                        }
+                        const mPause = await pause(e.fromPlayerId);
+                        console.log('==> mPause ', mPause);
+                    }, 10000);
+                } 
+            }, false );
+            pauseListener = pVideoPlayer.addListener( 'jeepCapVideoPlayerPause', async ( e ) => { 
+                console.log( "Event jeepCapVideoPlayerPause playerId " + 
+                e.fromPlayerId + " time " + e.currentTime);
+                if(apiCount.current === 0) {
+                    const mIsPlaying = await isPlaying(e.fromPlayerId);
+                    console.log("==> in Pause mIsPlaying " + mIsPlaying.value);
+                    const volume = await getVolume(e.fromPlayerId);
+                    if(volume.result) {
+                        console.log("==> volume " + volume.value);
+                    }                
+                    const currentTime = await getCurrentTime(e.fromPlayerId);
+                    if(currentTime.result) {
+                        console.log('==> currentTime ' + currentTime.value);
+                    }
+                    let muted = await getMuted(e.fromPlayerId);
+                    console.log("==> muted before setMuted " + muted.value);
+                    muted = await setMuted(e.fromPlayerId,!muted.value);
+                    console.log("==> setMuted " + muted.value);
+                    muted = await getMuted(e.fromPlayerId);
+                    console.log("==> muted after setMuted " + muted.value);
+                    apiTimer2.current = setTimeout(async () => {
+                        const duration = await getDuration(e.fromPlayerId);
+                        const rCurrentTime = await setCurrentTime(e.fromPlayerId,duration.value - 4);
+                        console.log('====> setCurrentTime ', rCurrentTime.value);
+                        await play(e.fromPlayerId);
+                    }, 4000);
+                }
+            }, false );
+            readyListener = pVideoPlayer.addListener( 'jeepCapVideoPlayerReady', ( e ) => { 
+                console.log( "Event jeepCapVideoPlayerReady  playerId " + 
+                e.fromPlayerId + " time " + e.currentTime); 
+            }, false );
+            exitListener = pVideoPlayer.addListener( 'jeepCapVideoPlayerExit', ( e ) => { 
+                console.log( "Event jeepCapVideoPlayerExit  playerId " + 
+                e.fromPlayerId + " time " + e.currentTime);
+                exitClear();
+            }, false );
+            endedListener = pVideoPlayer.addListener( 'jeepCapVideoPlayerEnded', ( e ) => {
+                console.log( "Event jeepCapVideoPlayerEnded  playerId " + 
+                e.fromPlayerId + " time " + e.currentTime); 
+                exitClear();
+            }, false );
+            if ( platform === "ios" || platform === "android" )
+                playerNative();
+            else
+                playerWeb(); 
+        
+        }
+
+        return () => {
+            console.log("$$$$$$$$ in cleanup listeners $$$$$$$$")
+            // Clean up the listeners
+            if(playListener != null) playListener.remove();
+            if(pauseListener != null) pauseListener.remove();
+            if(readyListener != null) readyListener.remove();
+            if(exitListener != null) exitListener.remove();
+            if(endedListener != null) endedListener.remove();
+        };
+        
+    }, [pVideoPlayer, initPlayer, play, isPlaying, pause, getDuration,
+        getVolume, setVolume, getCurrentTime, setCurrentTime, 
+        getMuted, setMuted, platform, url, first] )
+
+
     return (
-      <BrowserRouter>
-      <div>
-          <Route path="/" component={Home} exact/>
-          <Route path="/storedefault" component={StoreDefault} />
-          <Route path="/storeonedbonetable" component={StoreOneDBOneTable} />
-          <Route path="/storeonedbtwotables" component={StoreOneDBTwoTables} />
-          <Route path="/storetwodbstwotables" component={StoreTwoDBsTwoTables} />
-      </div> 
-    </BrowserRouter>
-  );
-  }
+        <div className="main-container">
+            {(first) &&
+                <div id="fullscreen-video" slot="fixed">
+                </div>
+            }
+        </div>
+    )
 }
-/*
-        <Switch>
-          <Route path="/" component={Home} exact/>
-        </Switch>
 
-*/
-const rootElement = document.getElementById("root") || document.createElement('div');  // for testing purposes
-ReactDOM.render(<App />, rootElement);
-
-export default App;
-
+export default VideoPlayer
